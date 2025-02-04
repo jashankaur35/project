cac8bd799ac04eb7659479b97a7ac4a16230dba2
jashanpreet kaur035 <113895705+jashankaur35@users.noreply.github.com>
2025-02-04 14:52:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <head>
   <title>Mini Project</title>
   <link rel="stylesheet" href="style.css">
+  
 </head>
   <body>
     <nav>
-      
+      <h2>jashanpreet kaur</h2>
     </nav>
   </body>
 </html>
